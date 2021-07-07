717bb5e009d610556bdf60d90dfae0d8ea2f7453
luohx <Lhongxiao@lcchain.com>
2021-07-07 18:42:15 +0800

    实现过滤消息

---
+++ simple-example/src/main/java/com/study/rocketmq/filter/sql/Producer.java
@@ -0,0 +1,49 @@
+package com.study.rocketmq.filter.sql;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.client.producer.SendStatus;
+import org.apache.rocketmq.common.message.Message;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author: luohx
+ * @Description: 过滤消息
+ * @Date: 2021/7/7 0007 17:27
+ */
+public class Producer {
+    public static void main(String[] args) throws Exception {
+        //a、创建消息生产者producer，并指定生产者组名
+        DefaultMQProducer producer = new DefaultMQProducer("group1");
+        //b、指定Nameserver地址
+        producer.setNamesrvAddr("49.232.217.148:9876;49.232.189.29:9876");
+        //c、启动producer
+        producer.start();
+        for (int i = 0; i < 10; i++) {
+            //d、创建消息对象，指定主题Topic、Tag和消息体
+            /**
+             * 参数一：消息主题Topic
+             * 参数二：消息Tag
+             * 参数三：消息内容
+             */
+            Message msg = new Message("FilterSQLTopic", "Tag1", ("Hello World" + i).getBytes());
+
+            msg.putUserProperty("i", String.valueOf(i));
+
+            //e、发送消息
+            SendResult result = producer.send(msg);
+            //发送状态
+            SendStatus status = result.getSendStatus();
+            //消息ID
+            String msgId = result.getMsgId();
+            //消息接收队列ID
+            int queueId = result.getMessageQueue().getQueueId();
+            System.out.println("发送状态：" + result + ",消息ID：" + msgId + "，队列：" + queueId);
+            //线程睡1s
+            TimeUnit.SECONDS.sleep(1L);
+        }
+//        f、关闭生产者producer
+        producer.shutdown();
+    }
+}
--- simple-example/src/main/java/com/study/rocketmq/filter/tag/Consumer.java
+++ simple-example/src/main/java/com/study/rocketmq/filter/tag/Consumer.java
@@ -1,16 +1,14 @@
-package com.study.rocketmq.base.consumer;
+package com.study.rocketmq.filter.tag;
 
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
-import org.apache.rocketmq.client.consumer.MessageSelector;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
 import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
 import org.apache.rocketmq.common.message.MessageExt;
-import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
 
 /**
  * @Author: luohx
- * @Description: 消息接收者
- * @Date: 2021/7/7 0007 10:11
+ * @Description: 描述
+ * @Date: 2021/7/7 0007 17:29
  */
 public class Consumer {
     public static void main(String[] args) throws Exception {
@@ -19,10 +17,8 @@ public class Consumer {
         //b、指定Nameserver地址
         mqConsumer.setNamesrvAddr("49.232.217.148:9876;49.232.189.29:9876");
         //c、订阅主题Topic和Tag
-        mqConsumer.subscribe("base", "Tag2");
+        mqConsumer.subscribe("FilterTagTopic", "Tag1 || Tag2");
 
-        //设定消费模式：负载均衡|广播模式
-        mqConsumer.setMessageModel(MessageModel.BROADCASTING);
         //d、设置回调函数，处理消费
         //接收消息内容
         mqConsumer.registerMessageListener((MessageListenerConcurrently) (msgs, consumeConcurrentlyContext) -> {
@@ -33,6 +29,6 @@ public class Consumer {
         });
         //e、启动消费者consumer
         mqConsumer.start();
-
+        System.out.println("消费者启动");
     }
 }
